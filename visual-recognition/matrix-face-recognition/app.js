7e7b35e721c2155b80c1b3e1455b26bbf014d5d0
Bilal Al-Saeedi <alronz.bilal@gmail.com>
2018-02-22 22:43:18 +0100

    Adding the first logic

---
+++ visual-recognition/matrix-face-recognition/app.js
@@ -0,0 +1,239 @@
+const uuid = require('uuid/v1');
+
+const AWSIoTClient = require('./utils/AWSIoTClient');
+const appConfig = require('./configs/config.defaults');
+const awsIoTClient = new AWSIoTClient();
+const timeout = 5;
+let lightingInterval;
+const colors = [
+  'red',
+  'cyan',
+  'orange',
+  'green'
+];
+
+function alongtime(n) {
+  return new Promise(function (resolve, reject) {
+    function loopTillDone() {
+      if (n) {
+        --n;
+        setTimeout(loopTillDone);
+      } else {
+        resolve();
+      }
+    }
+
+    loopTillDone();
+  });
+}
+
+function stopRecognitionService() {
+  console.log('stop recognition');
+  matrix.service('recognition', {timeout: timeout}).stop();
+  // a workaround to avoid the problem in the matrix os where process.on('message') create duplicates listeners
+  // https://github.com/matrix-io/matrix-os/issues/108
+  process.removeAllListeners('message');
+}
+
+function blink() {
+  return Promise.resolve()
+    .then(() => matrix.led(colors[Math.floor(Math.random() * colors.length)]).render())
+    .then(() => alongtime(2000))
+    .then(() => matrix.led('black').render())
+}
+
+function showColor(color) {
+  return Promise.resolve()
+    .then(() => matrix.led(color).render())
+    .then(() => alongtime(2000))
+    .then(() => matrix.led('black').render())
+}
+
+function startLighting() {
+  let a = 180;
+  let a2 = 0;
+  lightingInterval = setInterval(() => {
+    matrix.led([{
+      arc: Math.round(180 * Math.sin(a)),
+      color: 'darkgreen',
+      start: a2
+    }, {
+      arc: -Math.round(180 * Math.sin(a)),
+      color: 'darkgreen',
+      start: a2 + 180
+    }]).render();
+    a = (a < 0) ? 180 : a - 0.1;
+    //a2 = (a2 > 360) ? 0 : a2 + 5;
+  }, 25);
+}
+
+function stopLights() {
+  clearInterval(lightingInterval);
+}
+
+function clear() {
+  stopRecognitionService();
+  stopLights();
+  console.log('clear - completed');
+}
+
+function sendIAWSIoTMessage(topic, message) {
+  console.log('sendIAWSIoTMessage - send message to aws IoT');
+  awsIoTClient.publish(topic, message, {}, function () {
+    console.log(`message sent to IoT successfully`);
+    if (message.status === 'failed') {
+      showColor('red').then(() => clear());
+    } else {
+      showColor('green').then(() => clear())
+    }
+  });
+}
+
+function listTags() {
+  console.log('listTags - started');
+  return showColor('orange')
+    .then(() => {
+      try {
+        matrix.service('recognition', {timeout: timeout}).getTags()
+          .then((tags) => {
+            sendIAWSIoTMessage('recognition-tags', {status: 'success', tags: tags});
+          })
+      } catch (err) {
+        console.log('listTags - something went wrong, error: ' + err);
+        sendIAWSIoTMessage('recognition-tags', {status: 'failed'});
+      }
+    })
+}
+
+function resetTag(payload) {
+  console.log('resetTag - started, tag:', payload.tag);
+  return showColor('orange')
+    .then(() => {
+      try {
+        matrix.service('recognition', {timeout: timeout}).untrain(payload.tag);
+        sendIAWSIoTMessage('recognition-resetTag', {status: 'success', tags: [payload.tag]});
+      } catch (err) {
+        console.log('reset tag - something went wrong, error: ' + error);
+        sendIAWSIoTMessage('recognition-resetTag', {status: 'failed'});
+      }
+    })
+}
+
+function train(payload) {
+  console.log('train - started, tag:', payload.tag);
+  let trained = false;
+  return showColor('orange')
+    .then(() => {
+      try {
+        matrix.service('recognition', {timeout: timeout})
+          .train(payload.tag).then((data) => {
+          console.log('train - train data: ' + JSON.stringify(data));
+          if (!trained && data.hasOwnProperty('count')) {
+            console.log('training partially done');
+            // means it's partially done
+            matrix.led({
+              arc: Math.round(360 * (data.count / data.target)),
+              color: 'blue',
+              start: 0
+            }).render();
+          } else if (!trained) {
+            trained = true;
+            sendIAWSIoTMessage('recognition-trained', {status: 'success', results: data});
+          }
+        })
+      } catch (err) {
+        console.log('train - something went wrong, error: ' + error);
+        sendIAWSIoTMessage('recognition-trained', {status: 'failed'});
+      }
+    });
+}
+
+function recognize(payload) {
+  console.log('recognize - started');
+  startLighting();
+  return showColor('orange')
+    .then(() => {
+      try {
+        matrix.service('recognition', {timeout: timeout})
+          .start(payload.tag).then((data) => {
+          console.log('recognize - data', data);
+          let minDistanceFace = _.values(data.matches);
+          minDistanceFace = _.sortBy(minDistanceFace, ['score'])[0];
+          if (minDistanceFace.score < 0.85) {
+            sendIAWSIoTMessage('recognition-recognized', {status: 'success', results: data});
+          } else {
+            console.log('recognize - not recognized');
+            sendIAWSIoTMessage('recognition-recognized', {status: 'failed'});
+            stopLights();
+          }
+        })
+      } catch (err) {
+        stopLights();
+        console.log('recognize - something went wrong, error: ' + error);
+        sendIAWSIoTMessage('recognition-recognized', {status: 'failed'});
+      }
+    })
+}
+
+function resetAll() {
+  console.log('resetAll - started');
+  return showColor('orange')
+    .then(() => {
+      try {
+        matrix.service('recognition', {timeout: timeout}).getTags()
+          .then((tags) => {
+            console.log('resetAll - tags:', JSON.stringify(tags));
+            const promises = Object.keys(tags).map((key) => {
+              console.log('remove', key);
+              return matrix.service('recognition', {timeout: timeout}).untrain(tags[key]);
+            });
+            return Promise.all(promises);
+          })
+          .then(() => {
+            sendIAWSIoTMessage('recognition-resetAll', {status: 'success'});
+          });
+      } catch (err) {
+        console.log('resetAll - something went wrong, error: ' + error);
+        sendIAWSIoTMessage('recognition-resetAll', {status: 'failed'});
+      }
+    })
+}
+
+function start() {
+  return Promise.resolve()
+    .then(() => showColor('cyan'))
+    .then(() => awsIoTClient.connect(appConfig.awsIoTConfigs, uuid()))
+    .then(() => {
+      awsIoTClient.subscribe('listTags');
+      awsIoTClient.subscribe('recognize');
+      awsIoTClient.subscribe('train');
+      awsIoTClient.subscribe('resetTag');
+      awsIoTClient.subscribe('resetAll');
+      awsIoTClient.onMessage((topic, payload) => {
+        payload = JSON.parse(payload.toString());
+        console.log('listenerFunction - message received', topic, payload);
+        switch (topic) {
+          case 'listTags':
+            listTags();
+            break;
+          case 'recognize':
+            recognize(payload);
+            break;
+          case 'train':
+            train(payload);
+            break;
+          case 'resetTag':
+            resetTag(payload);
+            break;
+          case 'resetAll':
+            resetAll();
+            break;
+        }
+      });
+    })
+    .catch((error) => {
+      console.log('start - something went wrong, error: ' + error);
+    })
+}
+
+start();
---
+++ visual-recognition/matrix-face-recognition/test/app-test.js
@@ -0,0 +1,92 @@
+const config = require('../configs/config.defaults');
+const AWSIoTClient = require('../utils/AWSIoTClient');
+const uuid = require('uuid/v1');
+
+const awsIoTClient  = new AWSIoTClient();
+awsIoTClient.connect(config.awsIoTConfigs, uuid())
+  .then(() => {
+    awsIoTClient.subscribe('recognition-tags');
+    awsIoTClient.subscribe('recognition-trained');
+    awsIoTClient.subscribe('recognition-resetTag');
+    awsIoTClient.subscribe('recognition-resetAll');
+    awsIoTClient.subscribe('recognition-trained');
+    awsIoTClient.subscribe('recognition-recognized');
+  });
+
+describe('App', function () {
+  this.timeout(20000);
+  beforeEach(() => {
+  });
+
+  after(() => {
+  });
+
+  it("sent get tags request and received tags", (done) => {
+    awsIoTClient.publish("listTags", {
+      "message": 'Please list tags'
+    }, {}, () => {
+      console.log(`message sent to IoT successfully`);
+      awsIoTClient.onMessage((topic, payload) => {
+        if (topic === 'recognition-tags') {
+          console.log(`tags : ${payload}`);
+          done();
+        }
+      });
+    });
+  });
+  it("sent train request and received train results", (done) => {
+    awsIoTClient.publish("train", {
+      message: 'Please start training for tag',
+      tag: 'alya-test-face'
+    }, {}, () => {
+      console.log(`message sent to IoT successfully`);
+      awsIoTClient.onMessage((topic, payload) => {
+        if (topic === 'recognition-trained') {
+          console.log(`trained data : ${payload}`);
+          done();
+        }
+      });
+    });
+  });
+  it("sent reset tag request and received confirmation", (done) => {
+    awsIoTClient.publish("resetTag", {
+      message: 'Please start reset for tag',
+      tag: 'alya-test-face'
+    }, {}, () => {
+      console.log(`message sent to IoT successfully`);
+      awsIoTClient.onMessage((topic, payload) => {
+        if (topic === 'recognition-resetTag') {
+          console.log(`resetTag data : ${payload}`);
+          done();
+        }
+      });
+    });
+  });
+  it("sent reset all request and received confirmation", (done) => {
+    awsIoTClient.publish("resetAll", {
+      message: 'Please start reset all tags'
+    }, {}, () => {
+      console.log(`message sent to IoT successfully`);
+      awsIoTClient.onMessage((topic, payload) => {
+        if (topic === 'recognition-resetAll') {
+          console.log(`resetTag payload : ${payload}`);
+          done();
+        }
+      });
+    });
+  });
+  it("sent recognize request and received confirmation", (done) => {
+    awsIoTClient.publish("recognize", {
+      message: 'Please start recognize',
+      tag: 'alya-test-face'
+    }, {}, () => {
+      console.log(`message sent to IoT successfully`);
+      awsIoTClient.onMessage((topic, payload) => {
+        if (topic === 'recognition-recognized') {
+          console.log(`recognized data: ${payload}`);
+          done();
+        }
+      });
+    });
+  });
+});
