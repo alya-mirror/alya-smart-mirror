42519b9cde732f47e6f7154d1698d8beb74adcbb
Unknown <alialsaeedi19@gmail.com>
2018-07-03 12:26:25 +0200

    getting and deleting addons dynamically

--- src/index.js
+++ src/index.js
@@ -9,10 +9,79 @@ import React from 'react';
 import {render} from 'react-dom'
 import './index.css';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
-import ASMAnalogClock from '@alya-mirror/asm-analog-clock-addon'
+import io from 'socket.io-client';
+import ReactDOM from 'react-dom';
+import ASMyoutube from '@alya-mirror/asm-youtube-addon'
+import ASManalogClock from '@alya-mirror/asm-analog-clock-addon'
+import ASMDateTime from 'asm-date-time'
+import ASMGoogleImages from '@alya-mirror/asm-google-images-addon'
+
 
 render(<App/>, document.getElementById('root'));
-render(<ASMAnalogClock/>, document.getElementById('root').appendChild(document.createElement("div")));
+ReactDOM.unmountComponentAtNode(document.getElementById('root'));
 
-registerServiceWorker();
+let socket = io('http://localhost:3100');
+socket.on('connect', function () {
+  console.log('connected');
+});
+socket.on('disconnect', function () {
+  console.log('disconnected');
+});
+
+socket.on('addAddon', function (data) {
+
+  const message = JSON.parse(data).data;
+  const TagName = message.userAddon.name;
+  switch (TagName) {
+    case 'asmDateTime':
+      console.log(TagName + 'arrived');
+      let element1 = document.createElement('div');
+      element1.setAttribute("id", TagName);
+      render(<ASMDateTime/>, document.getElementById('root').appendChild(element1));
+      break;
+
+    case 'ASManalogClock':
+      console.log(TagName + 'arrived');
+      let element2 = document.createElement('div');
+      element2.setAttribute("id", TagName);
+      render(<ASManalogClock/>, document.getElementById('root').appendChild(element2));
+
+      break;
+    case 'ASMyoutube':
+      console.log(TagName + 'arrived');
+      let element3 = document.createElement('div');
+      element3.setAttribute("id", TagName);
+      render(<ASMyoutube/>, document.getElementById('root').appendChild(element3));
+
+      break;
+    default:
+      console.log('unfound name' + TagName);
+      break;
+  }
+});
+
+socket.on('deleteAddon', function (data) {
+//TODO delete using react way
+  const message = JSON.parse(data).data;
+  const TagName = message.userAddon.name;
+  switch (TagName) {
+    case 'asmDateTime':
+      console.log(TagName + 'to be deleted');
+      document.getElementById(TagName).remove();
+
+      break;
+    case 'ASManalogClock':
+      console.log(TagName + 'to be deleted');
+      document.getElementById(TagName).remove();
+
+      break;
+    case 'ASMyoutube':
+      console.log(TagName + 'to be deleted');
+      document.getElementById(TagName).remove();
+      break;
+
+    default:
+      console.log('unfound name' + TagName);
+      break;
+  }
+});
